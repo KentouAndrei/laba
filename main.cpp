698ed40d13104f6c44dada513a049fb6537fa390
andrey <andrey@Devops.myguest.virtualbox.org>
2024-11-23 12:12:09 +0200

    HTTP

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "FuncA.h"
-
+int CreateHTTPserver();
 int main() {
     FuncA func;
     std::cout << "Result (with argument): " << func.calculate(10) << std::endl;
+    
+    CreateHTTPserver();
+    
     return 0;
 }
